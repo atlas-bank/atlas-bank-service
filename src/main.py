c0fb47b2c860c1551ff4857b75d5e2923ef42efe
Guilherme Matte <guiguimatte@gmail.com>
2026-04-03 04:41:41 -0300

    Criado endpoint de check de saúde da aplicação

--- src/main.py
+++ src/main.py
@@ -1,36 +1,11 @@
-from View.LoginView import menu
-from View.LoginView import logincpf
-from View.LoginView import loginsenha
-from View.LoginView import saida
-from Controllers.SenhaAuth import raw_password_validator
-from Controllers.CPFAuth import CPFAutenticador
+from fastapi import FastAPI
 
-while True:
+app = FastAPI(
+    title="Atlas API",
+    version="1.0.0"
+)
 
-    opcao = menu()
 
-    if opcao == "1":
-        cpf = logincpf()
-
-        validacaoCPF = CPFAutenticador(cpf)
-
-        if validacaoCPF:
-            senha = loginsenha()
-            validacaoSenha = raw_password_validator(senha)
-
-            if validacaoSenha:
-                print("\nVocê fez Login!")
-                break
-
-            else:
-                print("\nNão fez login!")
-
-        elif validacaoCPF == False:
-            print("CPF Inválido!")
-
-    elif opcao == "0":
-        saida()
-        break
-
-    else:
-        print("\nOpção Inválida, tente novamente...")
+@app.get("/")
+def health():
+    return {"status": "ok"}
---
+++ src/models/user.py
@@ -0,0 +1,63 @@
+from dataclasses import dataclass
+
+from exceptions import BadRequestException
+from interfaces.entities_interface import IEntity
+
+@dataclass
+class User(IEntity):
+    collection = "users"
+
+    def __init__(
+            self,
+            full_name: str,
+            cpf: str,
+            birth_date: str,
+            parent_name: str,
+            rg: str,
+            rg_issuer: str,
+            rg_state: str,
+            email: str,
+            phone: str,
+            nationality: str,
+            marital_status: str,
+            gender: str,
+            zip_code: str,
+            address: str,
+            state: str,
+            country: str,
+            occupation: str = "",
+            salary: str = ""
+    ):
+        # Required fields
+        self.full_name = full_name
+        self.cpf = cpf
+        self.cpf_validator()
+
+        self.birth_date = birth_date
+        self.parent_name = parent_name
+        self.rg = rg
+        self.rg_issuer = rg_issuer
+        self.rg_state = rg_state
+        self.email = email
+        self.phone = phone
+        self.nationality = nationality
+        self.marital_status = marital_status
+        self.gender = gender
+        self.zip_code = zip_code
+        self.address = address
+        self.state = state
+        self.country = country
+
+        # Campos opcionais
+        self.occupation = occupation
+        self.salary = salary
+
+    def cpf_validator(self):
+        if not len(self.cpf) == 11 and not self.cpf.isdigit() and not self.cpf != "00000000000":
+            raise BadRequestException("Invalid CPF!")
+
+    def to_dict(self):
+        return self.__dict__
+
+    def get_collection(self):
+        return self.collection
