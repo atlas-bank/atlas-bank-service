31fecbf38584513df325bb01b3d7bedec8d1dbfb
Guilherme Matte <guiguimatte@gmail.com>
2026-04-03 21:06:27 -0300

    Ajustado Cors

--- src/core/app_factory.py
+++ src/core/app_factory.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, Depends
+from fastapi.middleware import cors
 
 from exceptions.handlers import register_exception_handlers
 from services.api_key_validate import validate_api_key
@@ -9,6 +10,14 @@ def create_app() -> FastAPI:
         title="Atlas API",
         version="1.0.0",
         dependencies=[Depends(validate_api_key)]
+    )
+
+    app.add_middleware(
+        cors.CORSMiddleware,
+        allow_origins=["*"],
+        allow_methods=["*"],
+        allow_headers=["*"],
+        allow_credentials=True,
     )
 
     register_exception_handlers(app)
